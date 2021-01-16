644bf3b5c0e684285a283e49534c3852a9b35e4f
Dilan <dunver33@gmail.com>
2021-01-16 15:35:14 +0300

    add features with html and css

---
+++ app.js
@@ -0,0 +1,91 @@
+// select items 
+const form = document.querySelector('.toDoList');
+const toDo = document.getElementById('toDo');
+const addBtn = document.querySelector('.add-btn');
+const container = document.querySelector('.toDoList-container');
+const list = document.querySelector('.toDoList-list');
+const clearBtn = document.querySelector('.clear-btn');
+
+// edit option
+editSymbol = false;
+let editElement = '';
+
+// event listeners
+form.addEventListener('submit', addItems);
+clearBtn.addEventListener('click', clearItems);
+
+// functions
+
+// add button
+function addItems(e){
+e.preventDefault();
+const value = toDo.value;
+const id = new Date().getTime().toString();
+  if(value && editSymbol){
+  editElement.innerHTML = value;
+  setBacktoDefault()
+} else if(value && !editSymbol){
+  createItems(id,value);
+  container.classList.add('show-container');
+  setBacktoDefault()
+}
+}
+// delete
+function deleteItems(e){
+  const element = e.currentTarget.parentElement.parentElement;
+  list.removeChild(element);
+  if(list.children.length === 0){
+    container.classList.remove('show-container')
+  }
+  setBacktoDefault();
+}
+// edit
+function editItems(e){
+  editElement = e.currentTarget.parentElement.previousElementSibling;
+  toDo.value = editElement.innerHTML; 
+  editSymbol = true;
+  addBtn.textContent = 'edit';
+}
+setBacktoDefault();
+
+// clear
+function clearItems(){
+  const items = document.querySelectorAll('.todolist-item')
+  if(items.length > 0){
+    items.forEach(item=>{
+      list.removeChild(item)
+    })
+  }
+  container.classList.remove('show-container');
+  setBacktoDefault()
+}
+
+
+function createItems(id,value){
+  const element = document.createElement('article');
+  element.classList.add('todolist-item');
+  const attr = document.createAttribute('data-id');
+  attr.value = id;
+  element.setAttributeNode(attr);
+  element.innerHTML = `<p class="title">${value}</p>
+  <div class="btn-container">
+    <button type="button" class="edit-btn">
+      <i class="far fa-edit fa-2x"></i>
+    </button>
+    <button type="button" class="delete-btn">
+      <i class="fas fa-times-circle fa-2x"></i>
+    </button>
+  </div>`
+  const deleteBtn = element.querySelector('.delete-btn');
+  deleteBtn.addEventListener('click', deleteItems);
+
+  const editBtn = element.querySelector('.edit-btn');
+  editBtn.addEventListener('click', editItems);
+  list.appendChild(element);
+}
+
+function setBacktoDefault(){
+  toDo.value = '';
+  addBtn.textContent = 'add';
+  editSymbol = false;
+}
